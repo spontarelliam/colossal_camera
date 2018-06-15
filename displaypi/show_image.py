7d1d6187f0cc808fc00e88639f4921b1f096deba
Adam Spontarelli <adam@vector-space.org>
2018-06-15 11:59:55 -0400

    added show_image.py

---
+++ displaypi/show_image.py
@@ -0,0 +1,14 @@
+import cv2
+import os, sys
+import time
+lastfile=''
+while True:
+    files = os.listdir("/home/pi/Pictures/")
+    if sorted(files)[-1] <> lastfile: # give file time to fully transfer
+        time.sleep(1)
+    lastfile = sorted(files)[-1]
+    print(lastfile)
+    img = cv2.imread("/home/pi/Pictures/"+lastfile)
+    cv2.imshow('img',img)
+    cv2.waitKey(1)
+cv2.destroyAllWindows()
