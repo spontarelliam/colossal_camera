0cc2a4f5c8bfc28e69cddd97a10a987c707e1f2f
Adam Spontarelli <adam@vector-space.org>
2018-06-19 22:20:47 -0400

    showimage

--- displaypi/show_image.py
+++ displaypi/show_image.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 import cv2
 import os, sys
 import time
 lastfile=''
 while True:
     files = os.listdir("/home/pi/Pictures/")
-    if sorted(files)[-1] <> lastfile: # give file time to fully transfer
+    if sorted(files)[-1] != lastfile: # give file time to fully transfer
         time.sleep(1)
     lastfile = sorted(files)[-1]
     print(lastfile)
