a303ac4a668ee9cb9f8a340120071f9cd49a7855
adam spontarelli <adam@vector-space.org>
2018-06-22 17:30:04 -0400

    replaced start script with systemd service. added logging and use of IP to camera script.

--- camerapi/camera.py
+++ camerapi/camera.py
@@ -11,17 +11,20 @@ import os
 import paramiko
 import datetime
 
+logfile = open("/home/pi/camera.log","a")
+
 GPIO.setmode(GPIO.BCM)
 camera = PiCamera()
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+ssh.load_host_keys(os.path.expanduser(os.path.join("/home/pi", ".ssh", "known_hosts")))
 
 def take_pic():
     print("taking picture")
     camera.rotation = 180
     filename = 'Cheers-'+datetime.datetime.now().strftime("%y-%m-%d-%s")+'.jpg'
     camera.capture('/home/pi/Pictures/'+filename)
+    logfile.writelines("picture taken")
     return filename
 
     
@@ -30,19 +33,25 @@ def send_pic(filename):
     try:
         sftp = ssh.open_sftp()
         sftp.put('/home/pi/Pictures/'+filename, '/home/pi/Pictures/'+filename)
-        print("Sent!")
+        sftp.put('/home/pi/camera.log', '/home/pi/camera.log')
         sftp.close()
+        print(filename+" was sent!")
+        logfile.writelines("picture sent")
     except:
         print("failed to send. red led")
 
         
 def main():
+    print("starting camera")
+    logfile.writelines("camera started")
     button=18
 
-
     try:
-        ssh.connect("displaypi", username="pi", password="cheers2018")
+        # dns not working
+        # ssh.connect("displaypi", username="pi", password="cheers2018")
+        ssh.connect("192.168.4.1", username="pi", password="cheers2018")
         print("connected")
+        logfile.writelines("connected to displaypi")
     except paramiko.SSHException:
         print("Connection to displaypi Failed")
     GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -51,11 +60,13 @@ def main():
         while True:
             print("waiting for button press")
             GPIO.wait_for_edge(button, GPIO.FALLING)
+            logfile.writelines("button pressed")
             filename = take_pic()
             send_pic(filename)
     except KeyboardInterrupt:
         GPIO.cleanup()
         ssh.close()
+        logfile.close()
     GPIO.cleanup()
 
 
